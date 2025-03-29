5183814286c8cca06b3dee91fe7a140604f44040
Conlombianos <160648563+conlombianos@users.noreply.github.com>
2025-03-29 20:51:56 -0300

    Create Questão1.py

---
+++ Questão1.py
@@ -0,0 +1,11 @@
+N1 = float(input())
+N2 = float(input())
+N3 = float(input())
+
+MediaA = ((N1 + N2 + N3)/ 3)
+MediaP = (((N1*2)+(N2*2)+(N3*3)) / 7)
+MediaP2 = (((N1*1)+(N2*2)+(N3*2)) / 5)
+
+print(f"Sua média aritimédica é:{MediaA}")
+print(f"Sua média aritimédica é:{MediaP}")
+print(f"Sua média aritimédica é:{MediaP2}")
