3fd3f6e134d34742783b3a32a284aa0a7c862d21
Conlombianos <160648563+conlombianos@users.noreply.github.com>
2025-03-29 23:22:05 -0300

    Delete Questão3.py

--- Questão3.py
+++
@@ -1,9 +0,0 @@
-valor_total = float(input("Digite o valor total das mercadorias compradas: R$ "))
-
-
-if valor_total <= 500:
-    print("Não há imposto a ser aplicado.")
-else:
-    excesso = valor_total - 500
-    imposto = excesso * 0.
-    print(f"O imposto a ser aplicado é de: R$ {imposto}")
